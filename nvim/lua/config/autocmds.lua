b2d1ac1df073fde7269ca2de11b1c1b9fb658754
todd <tp6gw94@gmail.com>
2023-05-25 07:51:48 +0800

    fix nav file id start 1

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -14,7 +14,7 @@ vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     local set = vim.keymap.set
     local ui = require("harpoon.ui")
-    for i = 0, 9 do
+    for i = 1, 9 do
       set("n", tostring(i), function()
         ui.nav_file(i)
       end, { desc = "Navigate to harpoon file " .. tostring(i) })
