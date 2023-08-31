41248393a5aa764e08e13d4f4b37478b549ad8f2
todd <tp6gw94@gmail.com>
2023-08-31 10:32:24 +0800

    config

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,2 +1,4 @@
 alias pn="pnpm"
 alias lg="lazygit"
+alias n="nnn"
+alias z="zellij"
--- fish/config.fish
+++ fish/config.fish
@@ -1,40 +1,8 @@
 
 source ~/.config/fish/alias.fish
 source ~/.config/fish/secret.fish
-if test "$TERM_PROGRAM" != WarpTerminal
-    # Existing bootstrap script here
-    # Fish syntax highlighting
-    set -g fish_color_autosuggestion 555 brblack
-    set -g fish_color_cancel -r
-    set -g fish_color_command --bold
-    set -g fish_color_comment red
-    set -g fish_color_cwd green
-    set -g fish_color_cwd_root red
-    set -g fish_color_end brmagenta
-    set -g fish_color_error brred
-    set -g fish_color_escape bryellow --bold
-    set -g fish_color_history_current --bold
-    set -g fish_color_host normal
-    set -g fish_color_match --background=brblue
-    set -g fish_color_normal normal
-    set -g fish_color_operator bryellow
-    set -g fish_color_param cyan
-    set -g fish_color_quote yellow
-    set -g fish_color_redirection brblue
-    set -g fish_color_search_match bryellow '--background=brblack'
-    set -g fish_color_selection white --bold '--background=brblack'
-    set -g fish_color_user brgreen
-    set -g fish_color_valid_path --underline
-    starship init fish | source
-else
-end
-# Configure Jump
-# status --is-interactive; and source (jump shell fish | psub)
 
-# Load all saved ssh keys
-# /usr/bin/ssh-add -A ^/dev/null
-
-
+set -Ux EDITOR hx
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 set -gx PATH /usr/local/bin $PATH
